5215365b2241cc518cc25a4fa77357846ec2079f
siiena25 <yuliacoyulia@yandex.ru>
2021-10-08 23:49:52 +0300

    Scripted auto-commit on change (2021-10-08 23:49:52) by gitwatch.sh

--- IdeaProjects/lab2/src/main/java/FlightWritableComparable.java
+++ IdeaProjects/lab2/src/main/java/FlightWritableComparable.java
@@ -1,5 +1,23 @@
 import org.apache.hadoop.io.WritableComparable;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class FlightWritableComparable implements WritableComparable<FlightWritableComparable> {
 
+    @Override
+    public int compareTo(FlightWritableComparable o) {
+        return 0;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
