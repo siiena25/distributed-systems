70c6d5ee3887e3e8dc20ec62b768ea1dcf8f9290
siiena25 <yuliacoyulia@yandex.ru>
2021-10-09 16:08:36 +0300

    Scripted auto-commit on change (2021-10-09 16:08:35) by gitwatch.sh

--- IdeaProjects/lab2/src/main/java/App.java
+++ IdeaProjects/lab2/src/main/java/App.java
@@ -1,2 +1,8 @@
 public class App {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: WordCountApp <input path> <output path>");
+            System.exit(-1);
+        }
+    }
 }
