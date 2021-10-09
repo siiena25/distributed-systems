9bd1a1d68f047dda04904fac666e77447f78f82b
siiena25 <yuliacoyulia@yandex.ru>
2021-10-09 23:25:21 +0300

    Scripted auto-commit on change (2021-10-09 23:25:21) by gitwatch.sh

--- IdeaProjects/lab2/src/main/java/start.sh
+++ IdeaProjects/lab2/src/main/java/start.sh
@@ -1,4 +1,5 @@
 mvn package
+hadoop fs -rm -r output
 hadoop fs -copyFromLocal 664600583_T_ONTIME_sample.csv
 hadoop fs -copyFromLocal L_AIRPORT_ID.csv
 export HADOOP_CLASSPATH=target/hadoop-examples-1.0-SNAPSHOT.jar
