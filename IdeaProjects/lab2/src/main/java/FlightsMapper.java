a970b640bbd53524355c04c70c7961775b63626b
siiena25 <yuliacoyulia@yandex.ru>
2021-10-09 16:56:43 +0300

    Scripted auto-commit on change (2021-10-09 16:56:43) by gitwatch.sh

--- IdeaProjects/lab2/src/main/java/FlightsMapper.java
+++ IdeaProjects/lab2/src/main/java/FlightsMapper.java
@@ -6,7 +6,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class FlightsMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-    
+
     private static final int AIRPORT_ID = 14;
     private static final int AIRPORT_DELAY = 18;
 
@@ -16,5 +16,14 @@ public class FlightsMapper extends Mapper<LongWritable, Text, Text, IntWritable>
             Text value,
             Mapper<LongWritable, Text, Text, IntWritable>.Context context) throws IOException, InterruptedException {
         super.map(key, value, context);
+        String[] line = value.toString().split(",");
+        if (key.get() > 0) {
+            String airportId = line[0].replaceAll("\"", "");
+            String code = line[1].replaceAll("\"", "");
+            context.write(
+                    new FlightWritableComparable(Integer.parseInt(airportId), 0),
+                    new Text(code)
+            );
+        }
     }
 }
