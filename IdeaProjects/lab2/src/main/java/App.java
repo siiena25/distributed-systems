48a9e3be92cddd908e4fbbb890898275c971da76
siiena25 <yuliacoyulia@yandex.ru>
2021-10-09 16:09:15 +0300

    Scripted auto-commit on change (2021-10-09 16:09:15) by gitwatch.sh

--- IdeaProjects/lab2/src/main/java/App.java
+++ IdeaProjects/lab2/src/main/java/App.java
@@ -4,5 +4,16 @@ public class App {
             System.err.println("Usage: WordCountApp <input path> <output path>");
             System.exit(-1);
         }
+        Job job = Job.getInstance();
+        job.setJarByClass(WordCountApp.class);
+        job.setJobName("Word count");
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.setMapperClass(WordMapper.class);
+        job.setReducerClass(WordReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
