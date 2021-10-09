fe707b5f4419d1a8828ada30d971052fe5c5c055
siiena25 <yuliacoyulia@yandex.ru>
2021-10-09 10:22:28 +0300

    Scripted auto-commit on change (2021-10-09 10:22:28) by gitwatch.sh

--- IdeaProjects/lab2/src/main/java/FlightMapper.java
+++ IdeaProjects/lab2/src/main/java/FlightMapper.java
@@ -14,7 +14,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, Text, FlightWritabl
         if (key.get() > 0) {
             String airportId = line[0].replaceAll("\"", "");
             String code = line[1].replaceAll("\"", "");
-            context.write(new FlightWritableComparable())
+            context.write(new FlightWritableComparable(airportId, 0), )
         }
         super.map(key, value, context);
     }
