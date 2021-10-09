9d4403586367bd9dcf1bccbd2c8aed24859ee7b8
siiena25 <yuliacoyulia@yandex.ru>
2021-10-09 17:23:59 +0300

    Scripted auto-commit on change (2021-10-09 17:23:59) by gitwatch.sh

--- IdeaProjects/lab2/src/main/java/App.java
+++ IdeaProjects/lab2/src/main/java/App.java
@@ -16,7 +16,8 @@ public class App {
         job.setJarByClass(App.class);
         job.setJobName("Join");
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportsMapper.class);
-        FileOutputFormat.setOutputPath(job, new Path(args[1]), TextInputFormat.class, FlightsMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlightsMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
         job.setMapperClass(AirportsMapper.class);
         job.setReducerClass(DelayReducer.class);
         job.setOutputKeyClass(Text.class);
