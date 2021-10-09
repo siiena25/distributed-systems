350eafe08462a49b59cb1754a3f370efd0ab59d7
siiena25 <yuliacoyulia@yandex.ru>
2021-10-09 14:26:03 +0300

    Scripted auto-commit on change (2021-10-09 14:26:03) by gitwatch.sh

--- IdeaProjects/lab2/src/main/java/AirportsPartitioner.java
+++ IdeaProjects/lab2/src/main/java/AirportsPartitioner.java
@@ -6,7 +6,6 @@ public class AirportsPartitioner extends Partitioner<FlightWritableComparable, T
 
     @Override
     public int getPartition(FlightWritableComparable flightWritableComparable, Text text, int partitionsNumber) {
-        
-        return 0;
+        return flightWritableComparable.getAirportId();
     }
 }
