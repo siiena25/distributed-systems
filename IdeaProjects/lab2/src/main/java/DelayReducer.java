6b1cff1428b53f1424a70de04e178568b24fe0d5
siiena25 <yuliacoyulia@yandex.ru>
2021-10-09 14:40:41 +0300

    Scripted auto-commit on change (2021-10-09 14:40:41) by gitwatch.sh

--- IdeaProjects/lab2/src/main/java/DelayReducer.java
+++ IdeaProjects/lab2/src/main/java/DelayReducer.java
@@ -1,4 +1,6 @@
+import org.apache.hadoop.mapreduce.Reducer;
+
 import javax.xml.soap.Text;
 
-public class DelayReducer extends FlightWritableComparable<FlightWritableComparable, Text, Text, Text> {
+public class DelayReducer extends Reducer<FlightWritableComparable, Text, Text, Text> {
 }
