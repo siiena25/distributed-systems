648441dae375e5963d103a1f6bbf4353a3930de8
siiena25 <yuliacoyulia@yandex.ru>
2021-10-09 14:24:48 +0300

    Scripted auto-commit on change (2021-10-09 14:24:48) by gitwatch.sh

--- IdeaProjects/lab2/src/main/java/AirportsPartitioner.java
+++ IdeaProjects/lab2/src/main/java/AirportsPartitioner.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class AirportsPartitioner {
+import org.apache.hadoop.mapreduce.Partitioner;
+
+import javax.xml.soap.Text;
+
+public class AirportsPartitioner extends Partitioner<FlightWritableComparable, Text> {
 }
