4a23874ac0766db5e269600e43440763a7d5ba0d
siiena25 <yuliacoyulia@yandex.ru>
2021-10-09 16:00:41 +0300

    Scripted auto-commit on change (2021-10-09 16:00:41) by gitwatch.sh

--- IdeaProjects/lab2/src/main/java/DelayReducer.java
+++ IdeaProjects/lab2/src/main/java/DelayReducer.java
@@ -24,6 +24,6 @@ public class DelayReducer extends Reducer<FlightWritableComparable, Text, Text, 
             max = Math.max(max, flightDelay);
             sum += flightDelay;
         }
-        context.write(new Text(airportName), );
+        context.write(new Text(airportName), new Text("Minimum delay time: " ));
     }
 }
