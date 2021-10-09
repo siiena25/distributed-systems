da3f8157042eb2381a6f5150747d14e00a7c28f8
siiena25 <yuliacoyulia@yandex.ru>
2021-10-09 16:39:30 +0300

    Scripted auto-commit on change (2021-10-09 16:39:30) by gitwatch.sh

--- IdeaProjects/lab2/src/main/java/FlightsMapper.java
+++ IdeaProjects/lab2/src/main/java/FlightsMapper.java
@@ -1,4 +1,16 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class FlightsMapper extends Mapper {
+import java.io.IOException;
+
+public class FlightsMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    @Override
+    protected void map(
+            LongWritable key,
+            Text value,
+            Mapper<LongWritable, Text, Text, IntWritable>.Context context) throws IOException, InterruptedException {
+        super.map(key, value, context);
+    }
 }
