3bebc0c9dac5dd0ab8ee8da8f818df19aee5923e
siiena25 <yuliacoyulia@yandex.ru>
2021-10-10 00:01:44 +0300

    Scripted auto-commit on change (2021-10-10 00:01:44) by gitwatch.sh

--- IdeaProjects/lab2/src/main/java/FlightWritableComparable.java
+++ IdeaProjects/lab2/src/main/java/FlightWritableComparable.java
@@ -4,7 +4,7 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
-public class FlightWritableComparable implements WritableComparable<FlightWritableComparable> {
+public class FlightWritableComparable implements WritableComparable {
 
     private int airportId;
     private int code;
