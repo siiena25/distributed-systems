a2bc9a1e2ca0a5a80dc2f014e87df3575be6b0b8
siiena25 <yuliacoyulia@yandex.ru>
2021-10-10 00:24:39 +0300

    Scripted auto-commit on change (2021-10-10 00:24:39) by gitwatch.sh

--- IdeaProjects/lab2/src/main/java/App.java
+++ IdeaProjects/lab2/src/main/java/App.java
@@ -18,6 +18,7 @@ public class App {
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlightsMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
         job.setPartitionerClass(AirportsPartitioner.class);
+        job.setGroupingComparatorClass(AirportsGroupingComparatorClass.class);
         job.setMapperClass(AirportsMapper.class);
         job.setReducerClass(DelayReducer.class);
         job.setMapOutputKeyClass(FlightWritableComparable.class);
