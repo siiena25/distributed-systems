c1d15549dbfdfd73a4bf69fa111b3d27f4b3cd64
siiena25 <yuliacoyulia@yandex.ru>
2021-10-09 14:33:11 +0300

    Scripted auto-commit on change (2021-10-09 14:33:11) by gitwatch.sh

--- IdeaProjects/lab2/src/main/java/AirportsGroupingComparatorClass.java
+++ IdeaProjects/lab2/src/main/java/AirportsGroupingComparatorClass.java
@@ -3,7 +3,7 @@ import org.apache.hadoop.io.WritableComparator;
 
 public class AirportsGroupingComparatorClass extends WritableComparator {
     @Override
-    public int compare(FlightWritableComparable a, FlightWritableComparable b) {
-        return super.compare(a, b);
+    public int compare(FlightWritableComparable firstItem, FlightWritableComparable secondItem) {
+        return Integer.compare(firstItem.getAirportId(), secondItem.getAirportId());
     }
 }
