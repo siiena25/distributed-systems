6f17730b97c1a087fb0d6da9434b2552c967a978
siiena25 <yuliacoyulia@yandex.ru>
2021-10-09 14:43:18 +0300

    Scripted auto-commit on change (2021-10-09 14:43:18) by gitwatch.sh

--- IdeaProjects/lab2/src/main/java/DelayReducer.java
+++ IdeaProjects/lab2/src/main/java/DelayReducer.java
@@ -1,5 +1,14 @@
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+
 public class DelayReducer extends Reducer<FlightWritableComparable, Text, Text, Text> {
+    @Override
+    protected void reduce(
+            FlightWritableComparable key,
+            Iterable<Text> values,
+            Reducer<FlightWritableComparable, Text, Text, Text>.Context context) throws IOException, InterruptedException {
+        super.reduce(key, values, context);
+    }
 }
