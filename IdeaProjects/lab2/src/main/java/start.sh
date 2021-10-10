d5a5bb897aaf72535bcd900629fab2282b0bed2c
siiena25 <yuliacoyulia@yandex.ru>
2021-10-10 12:26:15 +0300

    Scripted auto-commit on change (2021-10-10 12:26:15) by gitwatch.sh

--- IdeaProjects/lab2/src/main/java/start.sh
+++ IdeaProjects/lab2/src/main/java/start.sh
@@ -4,6 +4,6 @@ hadoop fs -rm -r output
 hadoop fs -copyFromLocal 664600583_T_ONTIME_sample.csv
 hadoop fs -copyFromLocal L_AIRPORT_ID.csv
 export HADOOP_CLASSPATH=target/hadoop-examples-1.0-SNAPSHOT.jar
-hadoop App L_AIRPORT_ID.csv 664600583_T_ONTIME_sample.csv output
+hadoop App 664600583_T_ONTIME_sample.csv L_AIRPORT_ID.csv output
 hadoop fs -copyToLocal output
 exit 0
