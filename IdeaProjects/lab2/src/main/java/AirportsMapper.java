176deab5b51c2df4b16f3c73fc25c7adb42d977b
siiena25 <yuliacoyulia@yandex.ru>
2021-10-10 10:46:35 +0300

    Scripted auto-commit on change (2021-10-10 10:46:35) by gitwatch.sh

--- IdeaProjects/lab2/src/main/java/AirportsMapper.java
+++ IdeaProjects/lab2/src/main/java/AirportsMapper.java
@@ -11,7 +11,7 @@ public class AirportsMapper extends Mapper<LongWritable, Text, FlightWritableCom
                        Text value,
                        Mapper<LongWritable, Text, FlightWritableComparable, Text>.Context context)
             throws IOException, InterruptedException {
-        String[] line = value.toString().split(",[]");
+        String[] line = value.toString().split(",[\"]");
         if (key.get() > 0) {
             String airportId = line[0].replaceAll("\"", "");
             String code = line[1].replaceAll("\"", "");
