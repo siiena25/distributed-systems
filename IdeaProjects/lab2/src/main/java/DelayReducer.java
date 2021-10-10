cc56988171943cacb7510a95929a581e500517ea
siiena25 <yuliacoyulia@yandex.ru>
2021-10-10 12:28:20 +0300

    Scripted auto-commit on change (2021-10-10 12:28:20) by gitwatch.sh

--- IdeaProjects/lab2/src/main/java/DelayReducer.java
+++ IdeaProjects/lab2/src/main/java/DelayReducer.java
@@ -11,24 +11,26 @@ public class DelayReducer extends Reducer<FlightWritableComparable, Text, Text, 
             Iterable<Text> values,
             Reducer<FlightWritableComparable, Text, Text, Text>.Context context) throws IOException, InterruptedException {
         Iterator<Text> iterator = values.iterator();
-        String airportName = iterator.next().toString();
-        float min = Float.MAX_VALUE;
-        float max = 0.f;
-        float sum = 0.f;
-        int count = 0;
-        while (iterator.hasNext()) {
-            float flightDelay = Float.parseFloat(String.valueOf(iterator.next()));
-            min = Math.min(min, flightDelay);
-            max = Math.max(max, flightDelay);
-            sum += flightDelay;
-            count += 1;
-            //context.write(new Text("\nflight delay: "), new Text(flightDelay + " " + min + " " + max + " " + count + " " + sum));
+        if (iterator.hasNext()) {
+            String airportName = iterator.next().toString();
+            float min = Float.MAX_VALUE;
+            float max = 0.f;
+            float sum = 0.f;
+            int count = 0;
+            while (iterator.hasNext()) {
+                float flightDelay = Float.parseFloat(String.valueOf(iterator.next()));
+                min = Math.min(min, flightDelay);
+                max = Math.max(max, flightDelay);
+                sum += flightDelay;
+                count += 1;
+                //context.write(new Text("\nflight delay: "), new Text(flightDelay + " " + min + " " + max + " " + count + " " + sum));
+            }
+            float average = sum / count;
+            context.write(
+                    new Text("\nAirport: " + airportName),
+                    new Text("\nMinimum delay time: " + min +
+                            "\nMaximum delay time: " + max +
+                            "\nAverage: " + average));
         }
-        float average = sum / count;
-        context.write(
-                new Text("\nAirport: " + airportName),
-                new Text("\nMinimum delay time: " + min +
-                        "\nMaximum delay time: " + max +
-                        "\nAverage: " + average));
     }
 }
