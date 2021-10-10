d7954d41aeda132059abd8781899620ce728d9ca
siiena25 <yuliacoyulia@yandex.ru>
2021-10-10 12:47:21 +0300

    Scripted auto-commit on change (2021-10-10 12:47:21) by gitwatch.sh

--- IdeaProjects/lab2/src/main/java/DelayReducer.java
+++ IdeaProjects/lab2/src/main/java/DelayReducer.java
@@ -18,7 +18,7 @@ public class DelayReducer extends Reducer<FlightWritableComparable, Text, Text, 
             float sum = 0.f;
             int count = 0;
             while (iterator.hasNext()) {
-                float flightDelay = Float.parseFloat(String.valueOf(iterator.next()));
+                float flightDelay = Float.parseFloat(iterator.next().toString());
                 min = Math.min(min, flightDelay);
                 max = Math.max(max, flightDelay);
                 sum += flightDelay;
