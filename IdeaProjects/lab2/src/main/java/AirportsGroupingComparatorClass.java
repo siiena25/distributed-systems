688703085cdcaaf9e782f4bdd870128cfeabc95d
siiena25 <yuliacoyulia@yandex.ru>
2021-10-11 07:41:50 +0300

    Scripted auto-commit on change (2021-10-11 07:41:50) by gitwatch.sh

--- IdeaProjects/lab2/src/main/java/AirportsGroupingComparatorClass.java
+++ IdeaProjects/lab2/src/main/java/AirportsGroupingComparatorClass.java
@@ -11,6 +11,6 @@ public class AirportsGroupingComparatorClass extends WritableComparator {
     public int compare(WritableComparable firstItem, WritableComparable secondItem) {
         FlightWritableComparable first = (FlightWritableComparable) firstItem;
         FlightWritableComparable second = (FlightWritableComparable) secondItem;
-        return first.compareByAirportId(second);
+        return first.compareTo(second);
     }
 }
