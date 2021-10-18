3a4ef569bdd91efa6b615ed239ba966d29990f90
julia <yulia2012394@gmail.com>
2021-10-18 09:27:32 +0300

    Scripted auto-commit on change (2021-10-18 09:27:32) by gitwatch.sh

--- lab3/src/main/java/flights/FlightsSerializable.java
+++ lab3/src/main/java/flights/FlightsSerializable.java
@@ -9,5 +9,45 @@ public class FlightsSerializable implements Serializable {
     private int cancelledFlights;
     private int numberOfFlights;
 
-    
+    public FlightsSerializable() {
+    }
+
+    public FlightsSerializable(float maxTimeOfDelay, int delayFlights, int cancelledFlights, int numberOfFlights) {
+        this.maxTimeOfDelay = maxTimeOfDelay;
+        this.delayFlights = delayFlights;
+        this.cancelledFlights = cancelledFlights;
+        this.numberOfFlights = numberOfFlights;
+    }
+
+    public float getMaxTimeOfDelay() {
+        return maxTimeOfDelay;
+    }
+
+    public void setMaxTimeOfDelay(float maxTimeOfDelay) {
+        this.maxTimeOfDelay = maxTimeOfDelay;
+    }
+
+    public int getDelayFlights() {
+        return delayFlights;
+    }
+
+    public void setDelayFlights(int delayFlights) {
+        this.delayFlights = delayFlights;
+    }
+
+    public int getCancelledFlights() {
+        return cancelledFlights;
+    }
+
+    public void setCancelledFlights(int cancelledFlights) {
+        this.cancelledFlights = cancelledFlights;
+    }
+
+    public int getNumberOfFlights() {
+        return numberOfFlights;
+    }
+
+    public void setNumberOfFlights(int numberOfFlights) {
+        this.numberOfFlights = numberOfFlights;
+    }
 }
