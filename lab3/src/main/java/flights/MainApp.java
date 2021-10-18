842ec38e964d4a5f0dd143068a191804d83d821b
julia <yulia2012394@gmail.com>
2021-10-18 10:02:33 +0300

    Scripted auto-commit on change (2021-10-18 10:02:33) by gitwatch.sh

--- lab3/src/main/java/flights/MainApp.java
+++ lab3/src/main/java/flights/MainApp.java
@@ -53,7 +53,7 @@ public class MainApp {
         return flightsData.combineByKey(
                 flight -> new FlightsSerializable(
                         flight.getDelay(),
-                        flight.getDelay() 
+                        flight.getDelay() > ZERO_DELAY ? 
                 )
         )
     }
