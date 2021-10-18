ad9518e4b32b5ac59f1416e7eed3c93610af8a33
julia <yulia2012394@gmail.com>
2021-10-18 10:30:07 +0300

    Scripted auto-commit on change (2021-10-18 10:30:07) by gitwatch.sh

---
+++ lab3/start.sh
@@ -0,0 +1,10 @@
+mvn package
+hadoop fs -rm -r output
+hadoop fs -put ~/L_AIRPORT_ID.csv
+hadoop fs -put ~/664600583_T_ONTIME_sample.csv
+export HADOOP_CLASSPATH=target/hadoop-examples-1.0-SNAPSHOT.jar
+hadoop App 664600583_T_ONTIME_sample.csv L_AIRPORT_ID.csv output
+hadoop jar ./target/hadoop-examples-1.0-SNAPSHOT.jar App 664600583_T_ONTIME_sample.csv L_AIRPORT_ID.csv output
+hadoop fs -copyToLocal output
+exit 0
+~           
