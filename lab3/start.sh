044661f779241850980b3b01d89521a00d0e72ec
julia <yulia2012394@gmail.com>
2021-10-18 11:17:38 +0300

    Scripted auto-commit on change (2021-10-18 11:17:38) by gitwatch.sh

--- lab3/start.sh
+++ lab3/start.sh
@@ -2,6 +2,6 @@ mvn package
 hadoop fs -rm -r output
 hadoop fs -put ~/L_AIRPORT_ID.csv
 hadoop fs -put ~/664600583_T_ONTIME_sample.csv
-spark-submit --class MainApp 664600583_T_ONTIME_sample.csv L_AIRPORT_ID.csv output --master yarn-client --num-executors 3 ./target/spark-examples-1.0-SNAPSHOT.jar
+spark-submit --class MainApp 664600583_T_ONTIME_sample.csv L_AIRPORT_ID.csv output --master yarn-client --num-executors 3 target/spark-examples-1.0-SNAPSHOT.jar
 hadoop fs -copyToLocal output
 exit 0
