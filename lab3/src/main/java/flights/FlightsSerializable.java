9ba4615451edb0c671caeb434262f40b1d58a184
julia <yulia2012394@gmail.com>
2021-10-18 09:26:28 +0300

    Scripted auto-commit on change (2021-10-18 09:26:28) by gitwatch.sh

--- lab3/src/main/java/flights/FlightsSerializable.java
+++ lab3/src/main/java/flights/FlightsSerializable.java
@@ -5,5 +5,7 @@ import java.io.Serializable;
 public class FlightsSerializable implements Serializable {
 
     private float maxTimeOfDelay;
+    private int delayFlights;
+    private int cancelledFlights;
     private int 
 }
