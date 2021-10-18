2ed313ce63349b9b011164921a738b4a1cef145b
julia <yulia2012394@gmail.com>
2021-10-18 08:56:50 +0300

    Scripted auto-commit on change (2021-10-18 08:56:50) by gitwatch.sh

--- lab3/src/main/java/flights/MainApp.java
+++ lab3/src/main/java/flights/MainApp.java
@@ -29,10 +29,18 @@ public class MainApp {
         JavaRDD<String> flightsFile = removeQuotes(sc.textFile(flights));
         JavaRDD<String> airportsFile = removeQuotes(sc.textFile(airports));
 
+        Map<Integer, String> flightsDataMap = flightsFile.mapToPair(MainApp::getFlightPairs).collectAsMap();
         Map<Integer, String> airportsDataMap = airportsFile.mapToPair(MainApp::getAirportPairs).collectAsMap();
 
         final Broadcast<Map<Integer, String>> airportsBroadcasted = sc.broadcast(airportsDataMap);
         
+    }
+
+    private static Tuple2<Integer, String> getFlightPairs(String line) {
+        int indexOfComma = line.indexOf(COMMA);
+        Integer airportID = Integer.valueOf(line.substring(0, indexOfComma));
+        String airportName = line.substring(indexOfComma + 1);
+        return new Tuple2<>(airportID, airportName);
     }
 
     private static Tuple2<Integer, String> getAirportPairs(String line) {
