b2a2858f5e06d47d2f4929422c52a155c6e25abc
julia <yulia2012394@gmail.com>
2021-10-18 08:31:30 +0300

    Scripted auto-commit on change (2021-10-18 08:31:30) by gitwatch.sh

--- lab3/src/main/java/flights/MainApp.java
+++ lab3/src/main/java/flights/MainApp.java
@@ -1,5 +1,8 @@
 package flights;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class MainApp {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
