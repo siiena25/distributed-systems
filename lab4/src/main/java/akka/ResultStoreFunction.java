2d56fac6e1d95a38cbeea939cd7c13551e5bd1b8
julia <yulia2012394@gmail.com>
2021-11-24 18:30:43 +0300

    Scripted auto-commit on change (2021-11-24 18:30:43) by gitwatch.sh

--- lab4/src/main/java/akka/ResultStoreFunction.java
+++ lab4/src/main/java/akka/ResultStoreFunction.java
@@ -1,8 +1,12 @@
 package akka;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class ResultStoreFunction {
     private final String packageId;
     private final String scripts;
     private final String functionTitle;
-    
+    private final ArrayList<Test>
 }
