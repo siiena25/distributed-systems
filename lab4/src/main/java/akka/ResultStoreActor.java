13a20a48f0c74b0fbe9f2e576ede965a8c7acc88
julia <yulia2012394@gmail.com>
2021-11-24 18:02:52 +0300

    Scripted auto-commit on change (2021-11-24 18:02:52) by gitwatch.sh

--- lab4/src/main/java/akka/ResultStoreActor.java
+++ lab4/src/main/java/akka/ResultStoreActor.java
@@ -3,10 +3,11 @@ package akka;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ResultStoreActor extends AbstractActor {
-    private Map<String, Map<String, String>> store = 
+    private Map<String, Map<String, String>> store = new HashMap<>();
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create().match(
