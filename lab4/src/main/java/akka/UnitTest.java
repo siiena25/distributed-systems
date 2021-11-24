4da16955dabba0a68c9ebaae844f53720c273087
julia <yulia2012394@gmail.com>
2021-11-24 18:24:26 +0300

    Scripted auto-commit on change (2021-11-24 18:24:26) by gitwatch.sh

--- lab4/src/main/java/akka/UnitTest.java
+++ lab4/src/main/java/akka/UnitTest.java
@@ -2,5 +2,7 @@ package akka;
 
 public class UnitTest {
     private final String packageId;
+    private final String script;
+    private final String testTitle;
     private final String packageId;
 }
