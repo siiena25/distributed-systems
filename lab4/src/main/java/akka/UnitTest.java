a40d06a0e09ccac420e6c02848d4f1b4d5c7f78a
julia <yulia2012394@gmail.com>
2021-11-24 18:24:59 +0300

    Scripted auto-commit on change (2021-11-24 18:24:59) by gitwatch.sh

--- lab4/src/main/java/akka/UnitTest.java
+++ lab4/src/main/java/akka/UnitTest.java
@@ -1,9 +1,14 @@
 package akka;
 
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.util.ArrayList;
+
 public class UnitTest {
     private final String packageId;
     private final String script;
     private final String testTitle;
     private final String funstionTitle;
     private final String result;
+    private final ArrayList<Integer> packageId;
 }
