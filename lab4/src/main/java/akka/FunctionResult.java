4afd3c42f6aae9eb06681298aec140d33729ba53
julia <yulia2012394@gmail.com>
2021-11-24 18:00:49 +0300

    Scripted auto-commit on change (2021-11-24 18:00:49) by gitwatch.sh

--- lab4/src/main/java/akka/FunctionResult.java
+++ lab4/src/main/java/akka/FunctionResult.java
@@ -10,6 +10,28 @@ public class FunctionResult {
         this.title = title;
         this.result = result;
     }
-
     
+    public String getPackageId() {
+        return packageId;
+    }
+
+    public void setPackageId(String packageId) {
+        this.packageId = packageId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
 }
