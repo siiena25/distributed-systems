deb3861ecdda834a695b809197259da300c88f03
julia <yulia2012394@gmail.com>
2021-11-24 18:10:45 +0300

    Scripted auto-commit on change (2021-11-24 18:10:45) by gitwatch.sh

--- lab4/src/main/java/akka/App.java
+++ lab4/src/main/java/akka/App.java
@@ -1,5 +1,6 @@
 package akka;
 
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 
 import java.io.IOException;
@@ -7,6 +8,6 @@ import java.io.IOException;
 public class App {
     public static void main(String[] args) throws IOException {
         ActorSystem system = ActorSystem.create();
-
+        ActorRef
     }
 }
