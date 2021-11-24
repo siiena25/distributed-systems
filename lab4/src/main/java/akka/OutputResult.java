023570f7be17122b1814948038886b468a51f4d0
julia <yulia2012394@gmail.com>
2021-11-24 18:08:21 +0300

    Scripted auto-commit on change (2021-11-24 18:08:21) by gitwatch.sh

--- lab4/src/main/java/akka/OutputResult.java
+++ lab4/src/main/java/akka/OutputResult.java
@@ -6,5 +6,24 @@ public class OutputResult {
     private String id;
     private Map<String, String> result;
 
-    public OutputResult(String id, )
+    public OutputResult(String id, Map<String, String> result) {
+        this.id = id;
+        this.result = result;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Map<String, String> getResult() {
+        return result;
+    }
+
+    public void setResult(Map<String, String> result) {
+        this.result = result;
+    }
 }
