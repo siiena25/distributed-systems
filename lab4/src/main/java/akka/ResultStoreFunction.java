5648b04bae3f2492b753a735e9712e914a993058
julia <yulia2012394@gmail.com>
2021-11-24 18:32:24 +0300

    Scripted auto-commit on change (2021-11-24 18:32:24) by gitwatch.sh

--- lab4/src/main/java/akka/ResultStoreFunction.java
+++ lab4/src/main/java/akka/ResultStoreFunction.java
@@ -7,12 +7,13 @@ import java.util.ArrayList;
 
 public class ResultStoreFunction {
     private final String packageId;
-    private final String scripts;
+    private final String script;
     private final String functionTitle;
     private final ArrayList<Test> tests;
 
     public ResultStoreFunction(
             @JsonProperty("packageId") String packageId,
-            @JsonProperty("script") String scripts,
+            @JsonProperty("script") String script,
+            
     )
 }
