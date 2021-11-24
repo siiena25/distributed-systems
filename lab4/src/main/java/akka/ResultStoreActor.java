bf5c64aed3957e754a845a27b6a358a691054612
julia <yulia2012394@gmail.com>
2021-11-24 17:57:58 +0300

    Scripted auto-commit on change (2021-11-24 17:57:58) by gitwatch.sh

--- lab4/src/main/java/akka/ResultStoreActor.java
+++ lab4/src/main/java/akka/ResultStoreActor.java
@@ -1,10 +1,11 @@
 package akka;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 public class ResultStoreActor extends AbstractActor {
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder.create()
     }
 }
