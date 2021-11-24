77e10d7eb26d19ae84cba58264356656232d596b
julia <yulia2012394@gmail.com>
2021-11-24 18:27:12 +0300

    Scripted auto-commit on change (2021-11-24 18:27:12) by gitwatch.sh

--- lab4/src/main/java/akka/UnitTest.java
+++ lab4/src/main/java/akka/UnitTest.java
@@ -13,6 +13,36 @@ public class UnitTest {
     private final ArrayList<Integer> params;
 
     public UnitTest(String packageId, String script, String testTitle, String functionTitle, String result, ArrayList<Integer> params) {
-        
+        this.packageId = packageId;
+        this.script = script;
+        this.testTitle = testTitle;
+        this.functionTitle = functionTitle;
+        this.result = result;
+        this.params = params;
+    }
+
+
+    public String getPackageId() {
+        return packageId;
+    }
+
+    public String getScript() {
+        return script;
+    }
+
+    public String getTestTitle() {
+        return testTitle;
+    }
+
+    public String getFunctionTitle() {
+        return functionTitle;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public ArrayList<Integer> getParams() {
+        return params;
     }
 }
