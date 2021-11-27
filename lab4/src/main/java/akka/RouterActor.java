a804f5dfc9f4b76c4fa4b9ec953b34c023e988c2
julia <yulia2012394@gmail.com>
2021-11-27 13:33:02 +0300

    Scripted auto-commit on change (2021-11-27 13:33:01) by gitwatch.sh

--- lab4/src/main/java/akka/RouterActor.java
+++ lab4/src/main/java/akka/RouterActor.java
@@ -4,7 +4,7 @@ import akka.actor.AbstractActor;
 import akka.actor.ActorSelection;
 
 public class RouterActor extends AbstractActor {
-    private ActorSelection testPerformer = 
+    private ActorSelection testPerformer = getContext().actorSelection()
     @Override
     public Receive createReceive() {
         return receiveBuilder().match(
