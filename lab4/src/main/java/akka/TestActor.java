65543933de56d567fde9db0744b058ec32f07253
julia <yulia2012394@gmail.com>
2021-11-27 15:11:56 +0300

    Scripted auto-commit on change (2021-11-27 15:11:56) by gitwatch.sh

--- lab4/src/main/java/akka/TestActor.java
+++ lab4/src/main/java/akka/TestActor.java
@@ -6,31 +6,37 @@ import akka.japi.pf.ReceiveBuilder;
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.util.ArrayList;
 
 public class TestActor extends AbstractActor {
     private final static String OK_RESULT = "OK";
     private final static String FAIL_RESULT = "FAIL";
     private final static String SCRIPT_NAME = "nashorn";
+
+    private ArrayList<Test> createTests(String script, String functionTitle, String testName,
+                                        String result, ArrayList<String> params) throws ScriptException, NoSuchMethodException {
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName(SCRIPT_NAME);
+        engine.eval(script);
+        Invocable invocable = (Invocable) engine;
+        String res = invocable.invokeFunction(functionTitle, params.toArray()).toString();
+        Test test = new Test(testName, result, params, result.equals(res));
+    }
+
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create().match(
                 UnitTest.class,
                 item -> {
-                    ScriptEngine engine = new ScriptEngineManager().getEngineByName(SCRIPT_NAME);
-                    engine.eval(item.getScript());
-                    Invocable invocable = (Invocable) engine;
-                    String result = invocable.invokeFunction(
-                            item.getFunctionTitle(),
-                            item.getParams().toArray()
-                    ).toString();
                     String answer = (result.equals(item.getResult())) ? OK_RESULT : FAIL_RESULT;
                     System.out.println("Expected result: " + item.getResult());
                     System.out.println("Received result: " + result);
                     System.out.println("Answer: " + answer);
                     sender().tell(new MessageStore(
                             item.getPackageId(),
-                            item.getTestName(),
-                            answer
+                            createTests(
+
+                            )
                     ), self());
                 }
         ).build();
