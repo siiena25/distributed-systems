e7d68466c4dccf893a8710e38d5ae73acae82c3d
julia <yulia2012394@gmail.com>
2021-11-27 13:38:09 +0300

    Scripted auto-commit on change (2021-11-27 13:38:09) by gitwatch.sh

--- lab4/src/main/java/akka/MessageTest.java
+++ lab4/src/main/java/akka/MessageTest.java
@@ -1,4 +1,42 @@
 package akka;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+
 public class MessageTest {
+    private final String packageId;
+    private final String script;
+    private final String functionTitle;
+    private final Test test;
+
+    @JsonCreator
+    public MessageTest(
+            @JsonProperty("packageId") String packageId,
+            @JsonProperty("jsScript") String script,
+            @JsonProperty("functionName") String functionTitle,
+            @JsonProperty("tests") Test test
+    ) {
+        this.packageId = packageId;
+        this.script = script;
+        this.functionTitle = functionTitle;
+        this.test = test;
+    }
+
+    public String getPackageId() {
+        return packageId;
+    }
+
+    public String getScript() {
+        return script;
+    }
+
+    public String getFunctionTitle() {
+        return functionTitle;
+    }
+
+    public Test getTests() {
+        return test;
+    }
 }
