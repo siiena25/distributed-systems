2bc5e3150ce7fc2a25d231a52c6a1d6d2beb55a4
julia <yulia2012394@gmail.com>
2021-11-28 22:22:31 +0300

    Scripted auto-commit on change (2021-11-28 22:22:31) by gitwatch.sh

--- lab4/src/main/java/akka/App.java
+++ lab4/src/main/java/akka/App.java
@@ -62,21 +62,3 @@ public class App {
         http.bindAndHandle(routeFlow, connect, actorMaterializer);
     }
 }
-
-/*
-{
-"packageId":"11",
-"jsScript":"var divideFn = function(a,b) { return a/b} ",
-"functionName":"divideFn",
-"tests": [
-{"testName":"test1",
-"expectedResult":"2.0",
-"params":[2,1]
-},
-{"testName":"test2",
-"expectedResult":"2.0",
-"params":[4,2]
-}
-]
-}
- */
