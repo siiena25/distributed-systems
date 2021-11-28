b49a73dc4b739ce3c4099b19646a9faecd8fd6fb
julia <yulia2012394@gmail.com>
2021-11-28 10:19:37 +0300

    Scripted auto-commit on change (2021-11-28 10:19:37) by gitwatch.sh

--- lab4/src/main/java/akka/MessageStore.java
+++ lab4/src/main/java/akka/MessageStore.java
@@ -1,12 +1,22 @@
 package akka;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.ArrayList;
 
 public class MessageStore {
+    @JsonProperty("packageId")
     private final Integer packageId;
+
+    @JsonProperty("test")
     private final ArrayList<Test> tests;
 
-    public MessageStore(Integer packageId, ArrayList<Test> tests) {
+    @JsonCreator
+    public MessageStore(
+            @JsonProperty("packageId") Integer packageId,
+            @JsonProperty("test") ArrayList<Test> tests
+    ) {
         this.packageId = packageId;
         this.tests = tests;
     }
