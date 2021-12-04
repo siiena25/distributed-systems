a93222531af9b740c9c134af79572ebc81214c01
julia <yulia2012394@gmail.com>
2021-12-04 17:12:57 +0300

    Scripted auto-commit on change (2021-12-04 17:12:57) by gitwatch.sh

--- lab5/src/main/java/akka_streams/TestResultParams.java
+++ lab5/src/main/java/akka_streams/TestResultParams.java
@@ -1,4 +1,19 @@
 package akka_streams;
 
 public class TestResultParams {
+    private final String url;
+    private final int count;
+
+    public TestResultParams(String url, int count) {
+        this.url = url;
+        this.count = count;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public int getCount() {
+        return count;
+    }
 }
