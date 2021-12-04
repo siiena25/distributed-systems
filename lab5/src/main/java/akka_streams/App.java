c90d4475e37eb365ce3753382c3d49b0485fb26a
julia <yulia2012394@gmail.com>
2021-12-04 18:06:30 +0300

    Scripted auto-commit on change (2021-12-04 18:06:30) by gitwatch.sh

--- lab5/src/main/java/akka_streams/App.java
+++ lab5/src/main/java/akka_streams/App.java
@@ -41,3 +41,5 @@ public class App {
 //http://localhost:8080/?testUrl=https://www.google.com/&count=20
 
 //http://localhost:8080/?testUrl=http://rambler.ru&count=20
+
+//http://localhost:8080/?testUrl=https://vk.com/&count=20
