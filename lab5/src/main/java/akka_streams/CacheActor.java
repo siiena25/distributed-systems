467a4c6560c6831da6271e7d1f938376fd99e220
julia <yulia2012394@gmail.com>
2021-12-04 17:54:54 +0300

    Scripted auto-commit on change (2021-12-04 17:54:54) by gitwatch.sh

--- lab5/src/main/java/akka_streams/CacheActor.java
+++ lab5/src/main/java/akka_streams/CacheActor.java
@@ -1,7 +1,10 @@
 package akka_streams;
 
 import akka.actor.AbstractActor;
+import akka.japi.Pair;
+
+import java.util.HashMap;
 
 public class CacheActor extends AbstractActor {
-    
+    private final HashMap<Pair<String, Integer>, TestResult>
 }
