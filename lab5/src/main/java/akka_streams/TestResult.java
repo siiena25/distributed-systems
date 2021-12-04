43eacc8b9a5c503d14ef6bf9be313eac999c5108
julia <yulia2012394@gmail.com>
2021-12-04 17:05:34 +0300

    Scripted auto-commit on change (2021-12-04 17:05:34) by gitwatch.sh

--- lab5/src/main/java/akka_streams/TestResult.java
+++ lab5/src/main/java/akka_streams/TestResult.java
@@ -3,6 +3,11 @@ package akka_streams;
 public class TestResult {
     private final String url;
     private final int requestsCount;
-    private final int resuestsTime;
-    
+    private final int requestsTime;
+
+    public TestResult(String url, int requestsCount, int requestsTime) {
+        this.url = url;
+        this.requestsCount = requestsCount;
+        this.requestsTime = requestsTime;
+    }
 }
