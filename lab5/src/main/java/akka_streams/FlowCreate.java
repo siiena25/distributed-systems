f15140a9036c5007a1a1be7c16ea0f260f52f9a0
julia <yulia2012394@gmail.com>
2021-12-04 17:16:44 +0300

    Scripted auto-commit on change (2021-12-04 17:16:44) by gitwatch.sh

--- lab5/src/main/java/akka_streams/FlowCreate.java
+++ lab5/src/main/java/akka_streams/FlowCreate.java
@@ -1,10 +1,11 @@
 package akka_streams;
 
 import akka.actor.ActorRef;
+import akka.stream.Materializer;
 
 public class FlowCreate {
     private final ActorRef cache;
-    private final Materiala
+    private final Materializer materializer;
 
     public FlowCreate() {
 
