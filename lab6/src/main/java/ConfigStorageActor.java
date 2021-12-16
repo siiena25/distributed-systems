4eef02819d231f3086a04d911546d20f49492bcd
julia <yulia2012394@gmail.com>
2021-12-16 23:23:20 +0300

    Scripted auto-commit on change (2021-12-16 23:23:20) by gitwatch.sh

--- lab6/src/main/java/ConfigStorageActor.java
+++ lab6/src/main/java/ConfigStorageActor.java
@@ -1,2 +1,16 @@
-public class ConfigStorageActor {
+import akka.actor.AbstractActor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ConfigStorageActor extends AbstractActor {
+    private List<String> listServers = new ArrayList<>();
+    private final Random random = new Random();
+
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
