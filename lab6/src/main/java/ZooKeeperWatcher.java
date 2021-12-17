e8d7bfd76ae9b8028ac9db9cff5e629cb5ee9186
julia <yulia2012394@gmail.com>
2021-12-17 07:23:49 +0300

    Scripted auto-commit on change (2021-12-17 07:23:49) by gitwatch.sh

--- lab6/src/main/java/ZooKeeperWatcher.java
+++ lab6/src/main/java/ZooKeeperWatcher.java
@@ -1,9 +1,39 @@
+import akka.actor.ActorRef;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ZooKeeperWatcher implements Watcher {
+    private final ZooKeeper zooKeeper;
+    private final ActorRef actorConf;
+
+    public ZooKeeper(ZooKeeper zooKeeper, ActorRef actorConf) {
+        this.zooKeeper = zooKeeper;
+        this.actorConf = actorConf;
+    }
     @Override
     public void process(WatchedEvent watchedEvent) {
+        try {
+            zooKeeper.getChildren("/servers", this);
 
+        } catch (KeeperException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendServers() {
+        List<String> servers = new ArrayList<>();
+        try {
+            for (String s : zooKeeper.getChildren("/servers", this)) {
+                servers.add(new String(zooKeeper.getData("/servers/" + s, false, null)));
+            }
+        } catch (KeeperException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        actorConf.tell(servers, ActorRef.noSender());
     }
 }
